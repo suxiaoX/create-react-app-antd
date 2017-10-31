01434157d7aa757fb693c395fb9f8ad2f5d232f1
leosu <leosu@shunshunliuxue.com>
2017-10-31 16:11:31 +0800

    ss

---
+++ config-overrides.js
@@ -0,0 +1,12 @@
+const { injectBabelPlugin } = require('react-app-rewired');
+const rewireLess = require('react-app-rewire-less');
+
+module.exports = function override(config, env) {
+  // do stuff with the webpack config...
+  // config = injectBabelPlugin(['import', { libraryName: 'antd', style: 'css' }], config);
+  // config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);
+  // config = rewireLess(config, env, {
+  //   modifyVars: { "@primary-color": "#1DA57A" },
+  // });
+  return config;
+};
